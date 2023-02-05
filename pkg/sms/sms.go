c6139a4d7955fea2aa6131261031e2b705a44ec0
edwinwalela <edwinwalela@gmail.com>
2023-02-05 12:02:22 +0300

    initialize sms send unit tests

--- pkg/sms/sms.go
+++ pkg/sms/sms.go
@@ -8,25 +8,25 @@ import (
 
 // BulkRequest represents the request body for the bulk SMS request
 type BulkRequest struct {
-	Username      string        // your africa's talking application username (required)
-	To            []string      // recipients` phone numbers "+2547XXXXXXXX"` (required)
-	Message       string        // message to be sent (required)
-	From          string        // your registered short code or alphanumerics, defaults to AFRICASTKNG (optional)
-	BulkSMSMode   bool          // determines who gets billed for a message sent, default value is true (sender is billed), must be true for bulk messages (optional)
+	Username      string        // Username is your africa's talking application username (required)
+	To            []string      // To specifies recipients` phone numbers "+2547XXXXXXXX"` (required)
+	Message       string        // Message is the contents of the sms to be sent (required)
+	From          string        // From is your registered short code or alphanumerics, defaults to AFRICASTKNG (optional)
+	BulkSMSMode   bool          // BulkSMSMode determines who gets billed for a message sent, default value is true (sender is billed), must be true for bulk messages (optional)
 	Enqueue       bool          // If enabled, the API will store the messages in a queue and send them out asynchronously after responding to the request (optional)
-	RetryDuration time.Duration // specifies the number of hours your subscription message should be retried in case it’s not delivered to the subscriber (optional)
+	RetryDuration time.Duration // RetryDuration specifies the number of hours your subscription message should be retried in case it’s not delivered to the subscriber (optional)
 }
 
 type PremiumRequest struct {
-	Username      string        // your africa's talking application username (required)
-	To            []string      // recipients` phone numbers "+2547XXXXXXXX"` (required)
-	Message       string        // message to be sent (required)
-	From          string        // your registered short code or alphanumerics, defaults to AFRICASTKNG (optional)
-	Keyword       string        // keyword to be used for a premium service (optional)
-	Enqueue       uint8         // If enabled, the API will store the messages in a queue and send them out asynchronously after responding to the request (optional)
-	LinkId        string        // Used to send OnDemand messages. The linkId is forwarded to your application when the user sends a message to your service. (optional)
-	RetryDuration time.Duration // specifies the number of hours your subscription message should be retried in case it’s not delivered to the subscriber (optional)
-	RequestId     string        // a client specified request identifier. Returned as part of the http dlr callback
+	Username      string        // Username your africa's talking application username (required)
+	To            []string      // To specifies recipients` phone numbers "+2547XXXXXXXX"` (required)
+	Message       string        // Message is the contents of the sms to be sent (required)
+	From          string        // From is your registered short code or alphanumerics, defaults to AFRICASTKNG (optional)
+	Keyword       string        // Keyword to be used for a premium service (optional)
+	Enqueue       bool          // If enabled, the API will store the messages in a queue and send them out asynchronously after responding to the request (optional)
+	LinkId        string        // LinkId is used to send OnDemand messages. The linkId is forwarded to your application when the user sends a message to your service. (optional)
+	RetryDuration time.Duration // RetryDuration specifies the number of hours your subscription message should be retried in case it’s not delivered to the subscriber (optional)
+	RequestId     string        // RequestId is a client specified request identifier. Returned as part of the http dlr callback
 }
 
 func SendBulk() {
--- pkg/sms/sms_test.go
+++ pkg/sms/sms_test.go
@@ -1,4 +1,33 @@
 package sms
 
+import (
+	"testing"
+	"time"
+)
 
+func TestSendBulk(t *testing.T) {
+	bulkRequest := &BulkRequest{
+		Username:      "",
+		To:            []string{""},
+		Message:       "Hello AT",
+		From:          "",
+		BulkSMSMode:   true,
+		RetryDuration: time.Hour,
+	}
+	_ = bulkRequest
+}
+
+func TestSendPremium(t *testing.T) {
+	premiumRequest := &PremiumRequest{
+		Username:      "",
+		To:            []string{""},
+		Message:       "Hello AT",
+		From:          "",
+		Keyword:       "",
+		Enqueue:       true,
+		LinkId:        "",
+		RetryDuration: time.Hour,
+		RequestId:     "",
+	}
+	_ = premiumRequest
 }
