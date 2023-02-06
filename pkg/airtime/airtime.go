21b415a588af01958163fd94350d2be2dffaf1f7
edwinwalela <edwinwalela@gmail.com>
2023-02-06 19:14:26 +0300

    update airtime package docs

--- pkg/airtime/airtime.go
+++ pkg/airtime/airtime.go
@@ -34,42 +34,42 @@ const (
 
 // Recipient represent the target user to receive airtime
 type Recipient struct {
-	PhoneNumber string  // PhoneNumber is the number to be topped up "+254xxxxxxx"
-	Amount      float64 // Amount is the value of airtime to send
-	Currency    string  // Currency is the currency code of the amount e.g KES,UGX,TZS,NGN,ETB,MWK,ZMW,ZAR
+	PhoneNumber string  // The number to be topped up "+254xxxxxxx"
+	Amount      float64 // Value of airtime to send
+	Currency    string  // Currency code of the amount e.g KES,UGX,TZS,NGN,ETB,MWK,ZMW,ZAR
 }
 
 // Request represents the request body for the Africa's talking airtime request
 type Request struct {
-	Recipients []Recipient // Recipients are the targets to be topped up
+	Recipients []Recipient // Targets to be topped up with airtime
 }
 
 // Transaction represents an individual airtime transaction result
 type Transaction struct {
 	PhoneNumber  string  // Phone number for this transaction
-	Amount       float64 // Amount is the value of airtime requested
-	Currency     string  // Currency is the currency code of the amount e.g KES,UGX,TZS,NGN,ETB,MWK,ZMW,ZAR
-	Discount     float64 // Discount is the discount applied to the requested airtime amount
+	Amount       float64 // Value of airtime requested
+	Currency     string  // Currency code of the amount e.g KES,UGX,TZS,NGN,ETB,MWK,ZMW,ZAR
+	Discount     float64 // Discount applied to the requested airtime amount
 	Status       string  // Status of the request associated to this phone number
-	RequestId    string  // RequestId is an identifier for the request to this phone number. Only generated if the status of the request is 'sent'
-	ErrorMessage string  // ErrorMessage is the error for the request associated with this phone number
+	RequestId    string  // An identifier for the request to this phone number. Only generated if the status of the request is 'sent'
+	ErrorMessage string  // Error message for the request associated with this phone number
 }
 
 // Response represents the response from Africa's Talking API
 type Response struct {
-	NumSent       int           // NumSent is the number of requests sent to the provider
-	TotalAmount   float64       // TotalAMount is the total value of airtime sent to the provider
-	Currency      string        // Currency is the currency code of the total amount e.g KES,UGX,TZS,NGN,ETB,MWK,ZMW,ZAR
-	TotalDiscount float64       // TotalDiscount is the total discount applied on the airtime
-	Responses     []Transaction // Responses is a list of the airtime transaction results
-	ErrorMessage  string        // ErrorMessage is the error message if the entire request was rejected by the API
+	NumSent       int           // Number of requests sent to the provider
+	TotalAmount   float64       // Total value of airtime sent to the provider
+	Currency      string        // Currency code of the total amount e.g KES,UGX,TZS,NGN,ETB,MWK,ZMW,ZAR
+	TotalDiscount float64       // Total discount applied on the airtime
+	Responses     []Transaction // A list of the airtime transaction results
+	ErrorMessage  string        // Error message if the entire request was rejected by the API
 }
 
 // Client represents the HTTP client responsible for communicating with Africa's Talking API
 type Client struct {
 	ApiKey    string       // API Key provided by Africa's talking
 	Username  string       // Your Africa's talking application username
-	IsSandbox bool         // IsSandbox specifies whether to use sandbox or live environment
+	IsSandbox bool         // Specifies whether to use sandbox or live environment
 	Client    *http.Client // HTTP client for making requests to Africa's Talking API
 }
 
