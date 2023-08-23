2506f67ab192838276e7b02da5d06fb1928f4f52
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-23 19:01:32 +0000

    example added to example package

--- examples/data/data.go
+++ examples/data/data.go
@@ -8,24 +8,18 @@ import (
 )
 
 func main() {
-	var apiKey = "4be4c57c242eefbcafe95f690f77d4ec1f2ec027df8c36320937006f8ddc3af2"
-	fmt.Println(apiKey)
 	client := &data.Client{
-		ApiKey:    apiKey,
-		Username:  "DanielSogbey",
+		ApiKey:    " ",
+		Username:  "",
 		IsSandbox: true,
 	}
 
 	dataRequest := &data.DataRequest{
-		Username:    client.Username,
-		ProductName: "Open Source Software",
+		Username:    "",
+		ProductName: "",
 		Recipients: []data.Recipient{
-			{PhoneNumber: "+233558159629"},
+			{PhoneNumber: "", Quantity: "", Unit: "", Validity: "", IsPromoBundle: "", MetaData: ""},
 		},
-		Quantity:      "1",
-		Unit:          "MB",
-		Validity:      "Day",
-		IsPromoBundle: "false",
 	}
 
 	response, err := client.SendMobileData(dataRequest)
