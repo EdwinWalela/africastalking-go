179eb59e6bc3d4b51c143f637e53b7b661fd27ff
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-09-01 15:08:52 +0000

    Proper documentation

--- pkg/data/data_test.go
+++ pkg/data/data_test.go
@@ -1,19 +1,20 @@
 package data
 
 import (
+	"os"
 	"testing"
 )
 
 func TestSendData(t *testing.T) {
 	client := &Client{
-		ApiKey:    "f0d2459e095f1bc43aaedcfb3dff0a45ff9c32e16794a9c21b170fbb6df1a657",
-		Username:  "DanielSogbey",
+		ApiKey:    os.Getenv("AT_API_KEY"),
+		Username:  os.Getenv("AT_USERNAME"),
 		IsSandbox: true,
 	}
 
 	dataRequest := &Request{
-		Username:    "DanielSogbey",
-		ProductName: "Open Source Software",
+		Username:    os.Getenv("AT_USERNAME"),
+		ProductName: os.Getenv("AT_PRODUCT_NAME"),
 		Recipients: []Recipient{
 			{PhoneNumber: "+233558159629", Quantity: 2, Unit: "MB", Validity: "Day", IsPromoBundle: "true", MetaData: ""},
 		},
